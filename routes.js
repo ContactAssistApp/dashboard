d12327b2227d0a55a9940c3a83a317c0be25741f
Kevin Gao <kevingao@microsoft.com>
2020-05-01 21:47:54 -0400

    cleanup routes

--- routes.js
+++ routes.js
@@ -1,55 +1,54 @@
 var express = require('express'),
-router = express.Router(),
-API = require('./src/controller')
+  router = express.Router(),
+  API = require('./src/controller')
 
-router.use(function (req,res,next) {
+router.use(function (req, res, next) {
   console.log(req.method + ' ' + req.path)
   next()
 })
 
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
   res.render('pages/index')
 })
 
-router.get('/design',function(req,res){ //MSR Design
+router.get('/design', function (req, res) { //MSR Design
   res.render('pages/design')
 })
 
-router.get('/dashboards',function(req,res){ //References
+router.get('/dashboards', function (req, res) { //References
   res.render('pages/dashboards')
 })
 
-router.get('/about',function(req,res){
+router.get('/about', function (req, res) {
   res.render('pages/about')
 })
 
-router.get('/contact',function(req,res){
+router.get('/contact', function (req, res) {
   res.render('pages/contact')
 })
 
 
-
 //API TEST
-router.get('/api/size',function(req,res){
+router.get('/api/size', function (req, res) { //todo head <> backend
   API.getSize(req.query.lat, req.query.lon, req.query.precision, req.query.lastTimestamp, (e) => {
     res.send(e)
   })
 })
 
-router.get('/api/list',function(req,res){
+router.get('/api/list', function (req, res) {
   API.getList(req.query.lat, req.query.lon, req.query.precision, req.query.lastTimestamp, (e) => {
     res.send(e)
   })
 })
 
-router.get('/api/message',function(req,res){ //todo post <> backend
-  API.postMessage('', (e) => {  
+router.get('/api/message', function (req, res) { //todo post <> backend
+  API.postMessage('', (e) => {
     res.send(e)
   })
 })
 
-router.get('/api/report',function(req,res){ //todo put <> backend
-  API.putAreaReport('', (e) => {  
+router.get('/api/report', function (req, res) { //todo put <> backend
+  API.putAreaReport('', (e) => {
     res.send(e)
   })
 })
