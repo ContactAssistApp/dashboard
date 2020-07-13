904e599added100046610fb3187aa788f3726219
David Jiang <davidjgrey@gmail.com>
2020-07-12 18:25:33 -0700

    require all fields filled before creating PSA

--- clientapp/src/components/TracerForm.jsx
+++ clientapp/src/components/TracerForm.jsx
@@ -27,10 +27,18 @@ export class TracerForm extends React.Component {
         this.onPublish = this.onPublish.bind(this);
     }
 
+    componentDidMount() {
+        let tracerForm = document.getElementById("tracer-form");
+        tracerForm.onsubmit = (ev) =>
+        {
+            ev.preventDefault();
+        }
+    }
+
     render() {
-        let sampleDescription = "Please refrain from entering playground Z until April 6th because it needs ot undergo decontamination.";
+        let sampleDescription = "Please refrain from entering playground Z until April 6th because it needs to undergo decontamination.";
         return (
-            <div className="tracer-form">
+            <form id="tracer-form">
                 <div>
                     <input type="text" value={this.state.type} onChange={this.onTypeChange} placeholder="PSA Type" className="tracer-form-type-select" required/>
                 </div>
@@ -48,20 +56,20 @@ export class TracerForm extends React.Component {
                     <Slider startingValue={10} label={"Radius"} unit={"miles"} min={0} max={100} />
                 </div>
                 <div className="tracer-form-dates">
-                    <input type="date" id="start-date" onChange={this.onStartDateChange}/>
+                    <input type="date" id="start-date" onChange={this.onStartDateChange} required/>
                     <span id="tracer-form-dates-separator">to</span>
-                    <input type="date" id="end-date" onChange={this.onEndDateChange}/>
+                    <input type="date" id="end-date" onChange={this.onEndDateChange} required/>
                 </div>
                 <div className="tracer-form-description">
                     <div>
-                        <textarea value={this.state.description} onChange={this.onDescriptionChange} placeholder={sampleDescription} className="tracer-form-textbox" />
+                        <textarea value={this.state.description} onChange={this.onDescriptionChange} placeholder={sampleDescription} className="tracer-form-textbox" required />
                     </div>
                 </div>
                 <div className="tracer-form-buttons">
                     <button className="cancel-button" onClick={this.onCancel}>Cancel</button>
                     <button className="publish-button" onClick={this.onPublish}>Publish</button>
                 </div>
-            </div>
+            </form>
         )
     }
 
@@ -122,6 +130,10 @@ export class TracerForm extends React.Component {
     }
 
     onPublish() {
-        this.props.onPublish();
+        // check for validation errors
+        let form = document.getElementById("tracer-form");
+        if (form.reportValidity()) {
+            this.props.onPublish();
+        }
     }
 }
