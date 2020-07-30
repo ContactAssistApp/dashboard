f3f0fd110adc1659561f26a863323c1d27a6e3ae
wangmeng255 <mengwan@microsoft.com>
2020-07-29 18:08:38 -0700

    Update dateTime getTimeZoneFormat

--- clientapp/src/utilities/dateTimeUtilites.js
+++ clientapp/src/utilities/dateTimeUtilites.js
@@ -1,5 +1,5 @@
 export const dateTime = {
-    getTimeZoneFormat: function(date){
+    getTimeZoneFormat: function(dateTime){
         const dateTimeFormat = new Intl.DateTimeFormat(
             'en-US',
             {
@@ -8,12 +8,16 @@ export const dateTime = {
                 day: '2-digit',
                 hour: '2-digit',
                 minute: '2-digit',
-                second: '2-digit'
+                second: '2-digit',
+                hourCycle: 'h23',
             }
         );
-        const [{ value: month },,{ value: day },,{ value: year },, {value: hour},,{value: minute},,{value:second}] = dateTimeFormat.formatToParts(date);
+       const dateTimeParts = dateTimeFormat.formatToParts(dateTime).reduce(function(acc, item){
+            acc[item['type']] = item['value'];
+            return acc;
+        }, {});
 
-        return `${year}${month}${day}T${hour}${minute}${second}`;
+        return `${dateTimeParts['year']}${dateTimeParts['month']}${dateTimeParts['day']}T${dateTimeParts['hour']}${dateTimeParts['minute']}${dateTimeParts['second']}`;
     },
     diffHours: function(dt2, dt1){
         var diff = (dt2 - dt1) / 1000;
