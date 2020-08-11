c4f1db70837871da11fe8aa38230debfdd93d314
wangmeng255 <mengwan@microsoft.com>
2020-08-11 12:22:38 -0700

    Move footer to app.js

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { LandingPage } from './components/LandingPage';
+import { Footer } from './components/Footer';
 
 class App extends React.Component {
   render() {
-    return <LandingPage />
+    return (
+      <div>
+        <LandingPage />
+        <Footer />
+      </div>
+    );
   }
   
 }
